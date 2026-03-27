8e087ba5da84ff931785d034dc6894804d23371d
Anderson Beauvais <50684207+Andy7911@users.noreply.github.com>
2026-03-26 22:45:32 -0400

    CardAlert Progress

--- src/features/dashboard/DashboardPage.tsx
+++ src/features/dashboard/DashboardPage.tsx
@@ -1,13 +1,20 @@
 
 import Card from "../dashboard/components/Card";
-
+import CardAlert from "./components/CardAlert";
 function DashboardPage() {
 
     return (
         <div className="p-2">
+            <div className="flex  justify-between">
             <Card numb={2} title="Aliment frais" subTitle="dans le frigo" ></Card>
-
+            <Card numb={2} title="Aliment frais" subTitle="dans le frigo" ></Card>
+            <Card numb={2} title="Aliment frais" subTitle="dans le frigo" ></Card>
+            <Card numb={2} title="Aliment frais" subTitle="dans le frigo" ></Card>
+            </div>
             <h1>Dashboard</h1>
+            <CardAlert  />
+
+      
         </div>
     )
 }
